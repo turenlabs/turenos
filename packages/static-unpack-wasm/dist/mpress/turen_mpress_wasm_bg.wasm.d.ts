70c0a6da47dee5371e6dfb700ea7624a4e25f121
Zeerg <8156339+Zeerg@users.noreply.github.com>
2026-09-22 14:15:08 +0000

    chore(wasm): rebuild static-unpack-wasm

--- packages/static-unpack-wasm/dist/mpress/turen_mpress_wasm_bg.wasm.d.ts
+++ packages/static-unpack-wasm/dist/mpress/turen_mpress_wasm_bg.wasm.d.ts
@@ -1,9 +1,9 @@
 /* tslint:disable */
 /* eslint-disable */
-export const memory: WebAssembly.Memory
-export const probe: (a: number, b: number, c: number) => void
-export const unpack_mpress: (a: number, b: number, c: number) => void
-export const unpack_mpress_metadata: (a: number, b: number, c: number) => void
-export const __wbindgen_add_to_stack_pointer: (a: number) => number
-export const __wbindgen_export: (a: number, b: number) => number
-export const __wbindgen_export2: (a: number, b: number, c: number) => void
+export const memory: WebAssembly.Memory;
+export const probe: (a: number, b: number, c: number) => void;
+export const unpack_mpress: (a: number, b: number, c: number) => void;
+export const unpack_mpress_metadata: (a: number, b: number, c: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number) => void;
