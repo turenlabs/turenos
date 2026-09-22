6e57b92864d531d91161ff264c7ef7d2c5ac11ee
Zeerg <8156339+Zeerg@users.noreply.github.com>
2026-09-22 14:12:54 +0000

    chore(wasm): rebuild stng-core-wasm

--- packages/stng-core-wasm/dist/turen_stng_core_wasm.d.ts
+++ packages/stng-core-wasm/dist/turen_stng_core_wasm.d.ts
@@ -1,25 +1,19 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export function extract(
-  bytes: Uint8Array,
-  min_length: number,
-  decode: boolean,
-  auto_xor: boolean,
-  xor_key: Uint8Array,
-): string
+export function extract(bytes: Uint8Array, min_length: number, decode: boolean, auto_xor: boolean, xor_key: Uint8Array): string;
 
-export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
-  readonly memory: WebAssembly.Memory
-  readonly extract: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => void
-  readonly __wbindgen_add_to_stack_pointer: (a: number) => number
-  readonly __wbindgen_export: (a: number, b: number) => number
-  readonly __wbindgen_export2: (a: number, b: number, c: number) => void
+    readonly memory: WebAssembly.Memory;
+    readonly extract: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => void;
+    readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+    readonly __wbindgen_export: (a: number, b: number) => number;
+    readonly __wbindgen_export2: (a: number, b: number, c: number) => void;
 }
 
-export type SyncInitInput = BufferSource | WebAssembly.Module
+export type SyncInitInput = BufferSource | WebAssembly.Module;
 
 /**
  * Instantiates the given `module`, which can either be bytes or
@@ -29,7 +23,7 @@ export type SyncInitInput = BufferSource | WebAssembly.Module
  *
  * @returns {InitOutput}
  */
-export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
 
 /**
  * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
@@ -39,6 +33,4 @@ export function initSync(module: { module: SyncInitInput } | SyncInitInput): Ini
  *
  * @returns {Promise<InitOutput>}
  */
-export default function __wbg_init(
-  module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>,
-): Promise<InitOutput>
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
